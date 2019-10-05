1f1ec2aeab5c402f4d328cc97b5643aabc03244c
senimankoding <doniasrulafandi@gmail.com>
2019-10-05 14:10:59 +0700

    first commit

---
+++ _sidebarr.php
@@ -0,0 +1,26 @@
+	<div id="sidebar">
+		<div class="sidebar-bars">
+			<p><a href="index.php">Seniman Koding</a></p>
+			<i class="fa fa-bars" aria-hidden="true"></i>
+			<i class="fa fa-times" aria-hidden="true"></i>
+		</div>
+		<div class="sidebar-title">
+			<div class="sidebar-title-img">
+				<img src="assets/img/profile.png" alt="image" class="img-fluid">
+			</div>
+			<div class="sidebar-title-texs">
+				JSON
+			</div>
+		</div>
+
+		<nav class="menu">
+			<h3>Fakultas</h3>
+		    <ul>
+		    	<li class="nav-link nav-link-color">Data</li>
+		    </ul>
+		</nav>
+
+		<div class="copyright">
+			© 2019 <a href="http://www.senimankoding.com/" target="_blank"></a>. All rights reserved
+		</div>
+	</div>
